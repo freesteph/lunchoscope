0d26d131505cdec2a7783e5c9e15cf869b1f659b
Stéphane Maniaci <stephane.maniaci@asos.com>
2019-04-26 13:44:13 +0100

    server: add a server to provide slack integration

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.listen(80, () => {
+  console.log("server started");
+});
